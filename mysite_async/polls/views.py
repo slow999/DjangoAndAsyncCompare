fb35f22ab56af83d1a4f2296272ed49c6deedd6c
yiqing-lan <slow999@gmail.com>
2022-11-27 20:19:24 -0600

    replace sleep with concrete function

--- mysite_async/polls/views.py
+++ mysite_async/polls/views.py
@@ -2,6 +2,18 @@ import asyncio
 from django.http import HttpResponse
 
 
+async def find_prime(start, stop):
+    prime_list = []
+    for val in range(start, stop):
+        if val > 1:
+            for i in range(2, int(val/2) + 1):
+                if val % i == 0:
+                    break
+            else:
+                prime_list.append(val)
+    return prime_list
+
+
 async def index(request):
-    await asyncio.sleep(2)
-    return HttpResponse("Hello, world. You're at the polls index.")
+    prime_numbers = await find_prime(1, 50000)
+    return HttpResponse(len(prime_numbers))
--- mysite_sync/polls/views.py
+++ mysite_sync/polls/views.py
@@ -2,6 +2,18 @@ import time
 from django.http import HttpResponse
 
 
+def find_prime(start, stop):
+    prime_list = []
+    for val in range(start, stop):
+        if val > 1:
+            for i in range(2, int(val/2) + 1):
+                if val % i == 0:
+                    break
+            else:
+                prime_list.append(val)
+    return prime_list
+
+
 def index(request):
-    time.sleep(2)
-    return HttpResponse("Hello, world. You're at the polls index.")
+    prime_numbers = find_prime(1, 50000)
+    return HttpResponse(len(prime_numbers))
