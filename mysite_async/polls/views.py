ecc2e7a9b7088250ca8f7f8e15feddb21abc3002
yiqing-lan <slow999@gmail.com>
2022-12-18 20:12:17 -0600

    clean up

--- mysite_async/polls/views.py
+++ mysite_async/polls/views.py
@@ -1,16 +1,15 @@
 from asgiref.sync import sync_to_async
 from polls.models import Foo
 from django.http import HttpResponse
-import asyncio
 
 
 def _find_count(value):
     return Foo.objects.filter(id__gte=value).count()
 
+
 find_count = sync_to_async(_find_count, thread_sensitive=True)
 
+
 async def index(request):
-    count1 = await find_count(50)
-    # count2 = await find_count(r'50')
-    # await asyncio.sleep(2)
-    return HttpResponse(count1)
+    count = await find_count(50)
+    return HttpResponse(count)
--- mysite_sync/polls/views.py
+++ mysite_sync/polls/views.py
@@ -1,10 +1,7 @@
 from polls.models import Foo
 from django.http import HttpResponse
-import time
 
 
 def index(request):
-    count1 = Foo.objects.filter(id__gte=50).count()
-    # count2 = Foo.objects.filter(a__regex=r'50').count()
-    # time.sleep(2)
-    return HttpResponse(count1)
+    count = Foo.objects.filter(id__gte=50).count()
+    return HttpResponse(count)
